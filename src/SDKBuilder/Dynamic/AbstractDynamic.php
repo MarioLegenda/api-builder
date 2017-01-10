cec80d1af568a0709f8be450ca1770f2a0e7be08
Mario Skrlec <whitepostmail@gmail.com>
2017-01-10 23:17:11 +0100

    Refactored AbstractDynamic to include UrlifyInterface

--- src/SDKBuilder/Dynamic/AbstractDynamic.php
+++ src/SDKBuilder/Dynamic/AbstractDynamic.php
@@ -2,7 +2,6 @@
 
 namespace SDKBuilder\Dynamic;
 
-use FindingAPI\Core\Helper;
 use SDKBuilder\Processor\UrlifyInterface;
 
 abstract class AbstractDynamic implements DynamicInterface, UrlifyInterface
@@ -45,62 +44,6 @@ abstract class AbstractDynamic implements DynamicInterface, UrlifyInterface
 
         return 'Errors: '.implode(', ', $this->exceptionMessages);
     }
-    /**
-     * @param int $counter
-     * @return string
-     */
-    public function urlify(int $counter) : string
-    {
-        $multipleValues = $this->configuration['multiple_values'];
-        $dateTime = $this->configuration['date_time'];
-
-        if ($multipleValues === false and $dateTime === false) {
-            $dynamicValue = $this->refactorDynamicValue($this->dynamicValue);
-
-            return 'itemFilter('.$counter.').name='.$this->name.'&itemFilter('.$counter.').value='.$dynamicValue[0].'&';
-        }
-
-        if ($multipleValues === true and $dateTime === false) {
-            $toBeAppended = 'itemFilter('.$counter.').name='.$this->name;
-
-            $internalCounter = 0;
-            foreach ($this->dynamicValue as $dynamic) {
-                $dynamicValue = $this->refactorDynamicValue((is_array($dynamic)) ? $dynamic : array($dynamic));
-
-                $toBeAppended.='&itemFilter('.$counter.').value('.$internalCounter.')='.$dynamicValue[0];
-
-                $internalCounter++;
-            }
-
-            return $toBeAppended.'&';
-        }
-
-        if ($multipleValues === false and $dateTime === true) {
-            $dateTime = $this->dynamicValue[0];
-
-            return 'itemFilter('.$counter.').name='.$this->name.'&itemFilter('.$counter.').value='.Helper::convertToGMT($dateTime).'&';
-        }
-
-        if ($multipleValues === true and $dateTime === true) {
-            $toBeAppended = 'itemFilter('.$counter.').name='.$this->name;
-
-            $internalCounter = 0;
-            foreach ($this->dynamicValue as $dynamic) {
-                $dynamicValue = '';
-                if ($dynamic instanceof \DateTime) {
-                    $dynamicValue = Helper::convertToGMT($dynamic);
-                } else {
-                    $dynamicValue = $this->refactorDynamicValue($dynamic);
-                }
-
-                $toBeAppended.='&itemFilter('.$counter.').value('.$internalCounter.')='.$dynamicValue[0];
-
-                $internalCounter++;
-            }
-
-            return $toBeAppended.'&';
-        }
-    }
 
     protected function genericValidation(array $value, $count = null) : bool
     {
@@ -130,17 +73,5 @@ abstract class AbstractDynamic implements DynamicInterface, UrlifyInterface
         }
 
         return true;
-    }
-
-    private function refactorDynamicValue(array $dynamics)
-    {
-        if (count($dynamics) === 1) {
-            $dynamic = $dynamics[0];
-            if (is_bool($dynamics[0])) {
-                return ($dynamic === true) ? array('true') : array('false');
-            }
-        }
-
-        return $dynamics;
     }
 }
